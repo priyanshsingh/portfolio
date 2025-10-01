27915e3a5d530b3a946316b793062cc51082f7c5
priyanshsingh <priyanshsingh1401@gmail.com>
2025-10-01 23:47:18 +0530

    Fix CV download issue - use public folder path

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,10 @@
 // call to action buttons
 import React from 'react'
-import CV from '../assets/CV.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={`${process.env.PUBLIC_URL}/assets/CV.pdf`} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
       
     </div>
